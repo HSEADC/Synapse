cf43998a6e3e6f8d896e4d5659826db9715922fb
“vitasiempre” <wavelolmu@gmail.com>
2022-11-24 15:11:08 +0400

    Colors

---
+++ src/plugin/color.js
@@ -0,0 +1,67 @@
+import { generateRandom } from './utilities'
+
+// create a var out of a function that will contain a palette
+// maybe need a promise
+// https://github.com/oxanakochueva/font/blob/master/src/app/App.jsx#L50
+// set and get
+
+function convertHSLtoRGB(h, s, l) {
+  s /= 100
+  l /= 100
+  const k = n => (n + h / 30) % 12
+  const a = s * Math.min(l, 1 - l)
+  const f = n => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)))
+
+  const colorRGB = {
+    r: String(f(0)).slice(0, 4) * 1,
+    g: String(f(8)).slice(0, 4) * 1,
+    b: String(f(4)).slice(0, 4) * 1
+  }
+
+  return colorRGB
+}
+
+function createBaseColor() {
+  const baseColorHSL = {
+    l: String(Math.random()).slice(0, 4) * 100,
+    s: String(Math.random()).slice(0, 4) * 100,
+    h: Math.floor(Math.random() * 360)
+  }
+
+  const baseColor = convertHSLtoRGB(
+    baseColorHSL.h,
+    baseColorHSL.s,
+    baseColorHSL.l
+  )
+  return baseColor
+}
+
+function adjustHue(val) {
+  if (val < 0) val += Math.ceil(-val / 360) * 360
+
+  return val % 360
+}
+
+function createScientificPalette() {
+  const targetHueSteps = {
+    analogous: [0, 30, 60],
+    triadic: [0, 120, 240],
+    tetradic: [0, 90, 180, 270],
+    complementary: [0, 180],
+    splitComplementary: [0, 150, 210]
+  }
+
+  const palette = {}
+
+  for (const type of Object.keys(targetHueSteps)) {
+    palettes[type] = targetHueSteps[type].map(step => ({
+      l: baseColor.l,
+      s: baseColor.s,
+      h: adjustHue(baseColor.h + step)
+    }))
+  }
+
+  return palette
+}
+
+export { createBaseColor, convertHSLtoRGB, createScientificPalette }
--- src/plugin/render.js
+++ src/plugin/render.js
@@ -1,5 +1,7 @@
 import { getNewFills } from './images'
 import { getStoreLanguage } from './store'
+import { generateRandom, convertHSLtoRGB } from './utilities'
+import { createBaseColor } from './color'
 
 function getImageBytesFromImagesForExportById(id) {
   const imagesForExport = getStoreImagesForExport()
@@ -14,38 +16,18 @@ function getImageBytesFromImagesForExportById(id) {
   return imageBytes
 }
 
-function renderMainFrame(background) {
-  //фрейм самого экспорта
-  let frame = figma.createFrame()
-  frame.x = figma.viewport.center.x
-  frame.y = figma.viewport.center.y
-  frame.paddingTop = 23
-  frame.itemSpacing = 23
-  frame.resize(768, 1659)
-  frame.layoutMode = 'VERTICAL'
-  frame.primaryAxisSizingMode = 'AUTO'
-  frame.counterAxisSizingMode = 'FIXED'
-  frame.counterAxisAlignItems = 'CENTER'
-  frame.fills = background
-
-  return frame
-}
-
 function renderFigmaTemplate(imagesForExport) {
-  const background = [
-    { type: 'SOLID', color: { r: 0.965, g: 0.956, b: 0.952 } }
-  ]
-
-  const mainFrame = renderMainFrame(background)
-
   figma.currentPage.selection = mainFrame
   figma.viewport.scrollAndZoomIntoView(mainFrame)
 }
 
 function createDesignFrame() {
   const frame = figma.createFrame()
+  const color = createBaseColor()
   frame.resize(1080, 1080)
-  frame.fills = [{ type: 'SOLID', color: { r: 0.06, g: 0.09, b: 0.11 } }]
+  frame.fills = [
+    { type: 'SOLID', color: { r: color.r, g: color.g, b: color.b } }
+  ]
   frame.layoutGrids = [
     { pattern: 'GRID', sectionSize: 27, color: { r: 1, g: 1, b: 1, a: 0.2 } },
     {
